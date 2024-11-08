18cd684885da15a27f51d70f631bb030d7664a82
Jacky Chung <clashofjacky@email.com>
2024-11-09 02:52:17 +0800

    Updates

--- assets/decimals-edfdb9d9.js
+++
@@ -1,1 +0,0 @@
-import{ct as n,r}from"./index-206aabae.js";const c="0x313ce567",o=[],e=[{type:"uint8"}];function s(t){return n({availableSelectors:t,method:[c,o,e]})}async function d(t){return r({contract:t.contract,method:[c,o,e],params:[]})}export{d,s as i};
---
+++ assets/eth_getTransactionCount-f25538fb.js
@@ -0,0 +1,1 @@
+import{ai as n,cG as c}from"./index-44008aae.js";async function u(t,o){const e=await t({method:"eth_getTransactionCount",params:[o.address,o.blockNumber?n(o.blockNumber):o.blockTag||"pending"]});return c(e)}export{u as eth_getTransactionCount};
